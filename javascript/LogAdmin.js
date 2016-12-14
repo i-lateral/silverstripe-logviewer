f9a7fe8affb2d2e69e7d72fae63208e18dfc73d8
Anselm Christophersen <ac@anselm.dk>
2016-12-14 02:36:10 +0100

    overriding SilverStripe's AJAX loading that goes nowhere

--- javascript/LogAdmin.js
+++ javascript/LogAdmin.js
@@ -9,3 +9,10 @@ function resizeWindow( e ) {
 	var RightHeight	= (parseInt(jQuery('.cms-content').height()) - 180);
 	jQuery("#right .Logs").css("height", RightHeight );
 }
+
+//overriding SilverStripe's AJAX loading that goes nowhere
+jQuery('.Logs .Nav a').click(function(e){
+  e.preventDefault();
+  var url = jQuery(this).attr('href');
+  location.href = url;
+});
